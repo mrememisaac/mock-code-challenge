fd5ee0c5750826bf74e6ee61a28cff76e4056345
Emem Isaac <me@ememisaac.com>
2021-03-22 10:07:12 +0100

    Add a method to generate sample departments in the  Employee test class

--- VogCodeChallenge.Tests/EmployeeServiceTest.cs
+++ VogCodeChallenge.Tests/EmployeeServiceTest.cs
@@ -1,9 +1,18 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using VogCodeChallenge.Entities;
 
 namespace VogCodeChallenge.Tests
 {
     public class EmployeeServiceTest
     {
-       
+        private List<Department> CreateDepartmentSampleData()
+        {
+            return new List<Department>
+            {
+                new Department{ Id = 1, Name ="Engineering", Address = "Developers Avenue"},
+                new Department{ Id=2, Name="Sales", Address="Sales Drive"}
+            };
+        }
+
     }
 }
