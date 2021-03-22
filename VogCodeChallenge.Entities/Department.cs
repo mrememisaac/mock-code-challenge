21347c3a0b569f195c4f3a1068654e9c6d7dda1e
Emem Isaac <me@ememisaac.com>
2021-03-22 07:21:49 +0100

    Create Department Entity

---
+++ VogCodeChallenge.Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
