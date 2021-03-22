0cd0803c55fb9fcd67d872ef9272b0a5351f100f
Emem Isaac <me@ememisaac.com>
2021-03-22 10:59:48 +0100

    Create a constructor
    The constructor request an ILogger for logging and an IEmployee Service for the core work it does - serve employee data

--- VogCodeChallenge.API/Controllers/EmployeesController.cs
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VogCodeChallenge.Services;
 
 namespace VogCodeChallenge.API.Controllers
 {
@@ -11,6 +13,13 @@ namespace VogCodeChallenge.API.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly ILogger<EmployeesController> _logger;
+        private readonly IEmployeeService _employeeService;
 
+        public EmployeesController(ILogger<EmployeesController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
     }
 }
