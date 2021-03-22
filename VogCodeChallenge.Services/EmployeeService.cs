916969a926d9dad8489fdd5b035e329c2838b528
Emem Isaac <me@ememisaac.com>
2021-03-22 09:07:34 +0100

    Add a method to create sample employee data

--- VogCodeChallenge.Services/EmployeeService.cs
+++ VogCodeChallenge.Services/EmployeeService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using VogCodeChallenge.Entities;
 
 namespace VogCodeChallenge.Services
@@ -27,6 +27,25 @@ namespace VogCodeChallenge.Services
             {
                 new Department{ Id = 1, Name ="Engineering", Address = "Developers Avenue"},
                 new Department{ Id=2, Name="Sales", Address="Sales Drive"}
+            };
+        }
+
+        private List<Employee> CreateSampleEmployees(List<Department> departments)
+        {
+            return new List<Employee>
+            {
+                new Employee
+                {
+                    Id =1, FirstName = "Emem", LastName="Isaac", JobTitle="Software Engineer", Address="4th Avenue", DepartmentId = departments.First().Id 
+                },
+                new Employee
+                {
+                    Id =1, FirstName = "Kim", LastName="Doran", JobTitle="Software Engineer", Address="5th Avenue", DepartmentId = departments.First().Id
+                },
+                new Employee
+                {
+                    Id =1, FirstName = "Kim", LastName="Miran", JobTitle="Sales Executive", Address="6th Avenue", DepartmentId = departments.Last().Id
+                },
             };
         }
     }
