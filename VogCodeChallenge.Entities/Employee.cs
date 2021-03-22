66836dfd1e7059ba219b51a7e6f4014cf219b221
Emem Isaac <me@ememisaac.com>
2021-03-22 07:36:16 +0100

    Create the Employee class
    This is based off Entity Base and implements IEntityBase. The department id property is how we tie an employee to his department

---
+++ VogCodeChallenge.Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Entities
+{
+    public class Employee : EntityBase, IEntityBase
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+    }
+}
