a012bec893191132a42be0feb2a2000d0dd98973
Emem Isaac <me@ememisaac.com>
2021-03-22 12:59:03 +0100

    Create EmployeesApiViewModel

---
+++ VogCodeChallenge.Entities/EmployeesApiViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Entities
+{
+    public class EmployeesApiViewModel
+    {
+        public int TotalRecordCount { get; set; }
+
+        public int Page { get; set; }
+
+        public int RecordsPerPage { get; set; }
+
+        public List<Employee> Data { get; set; }
+    }
+}
