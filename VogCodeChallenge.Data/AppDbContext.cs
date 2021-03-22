cf1835002d0f30a37fd66b36d21dec34bfce6150
Emem Isaac <me@ememisaac.com>
2021-03-22 14:20:24 +0100

    Create the application db context class

---
+++ VogCodeChallenge.Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VogCodeChallenge.Entities;
+
+namespace VogCodeChallenge.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
