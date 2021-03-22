7215e5fef07c054247b5f0c05b73832b274bd0de
Emem Isaac <me@ememisaac.com>
2021-03-22 14:52:18 +0100

    Make the EFEmployeeService implement IEmployeeService
    None of the methods have been properly implemented yet

--- VogCodeChallenge.Services/EFEmployeeService.cs
+++ VogCodeChallenge.Services/EFEmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ using VogCodeChallenge.Entities;
 
 namespace VogCodeChallenge.Services
 {
-    public class EFEmployeeService
+    public class EFEmployeeService : IEmployeeService
     {
         private readonly DbSet<Employee> _employees;
         private readonly DbSet<Department> _departments;
@@ -17,6 +18,26 @@ namespace VogCodeChallenge.Services
         {
             _employees = context.Employees;
             _departments = context.Departments;
+        }
+
+        public IEnumerable GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeesApiViewModel GetAll(int page, int recordsPerPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Employee> GetEmployeesByDepartment(int departmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList ListAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
