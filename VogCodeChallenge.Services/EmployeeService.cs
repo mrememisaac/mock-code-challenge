d99dc80e70f5a86fe3ba55989dede583b68115d9
Emem Isaac <me@ememisaac.com>
2021-03-22 08:45:01 +0100

    Create the EmployeeService class

---
+++ VogCodeChallenge.Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public IEnumerable GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList ListAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
