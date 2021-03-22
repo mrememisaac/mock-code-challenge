cf6c1bfa190ca4d44405b3dc9a903e2cd2a51caa
Emem Isaac <me@ememisaac.com>
2021-03-22 08:43:28 +0100

    Create the IEmployeeService interface

---
+++ VogCodeChallenge.Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Services
+{
+    public interface IEmployeeService
+    {
+        IEnumerable GetAll();
+
+        IList ListAll();
+    }
+}
