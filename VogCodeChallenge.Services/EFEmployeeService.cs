de04c60b82d5508fa5df79abf3edd3abf23a052e
Emem Isaac <me@ememisaac.com>
2021-03-22 14:50:12 +0100

    Create the EntityFramework version of EmployeeService

---
+++ VogCodeChallenge.Services/EFEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VogCodeChallenge.Data;
+using VogCodeChallenge.Entities;
+
+namespace VogCodeChallenge.Services
+{
+    public class EFEmployeeService
+    {
+        private readonly DbSet<Employee> _employees;
+        private readonly DbSet<Department> _departments;
+
+        public EFEmployeeService(AppDbContext context)
+        {
+            _employees = context.Employees;
+            _departments = context.Departments;
+        }
+    }
+}
