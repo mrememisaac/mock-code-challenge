8301df3c1811bbe05e0af0066d0e45c523c943d1
Emem Isaac <me@ememisaac.com>
2021-03-22 07:40:07 +0100

    Add an Employees collection property to Department class
    The employee collection is initialized in the constructor to avoid a client trying to populate a collection that does not exist

--- VogCodeChallenge.Entities/Department.cs
+++ VogCodeChallenge.Entities/Department.cs
@@ -6,5 +6,11 @@ namespace VogCodeChallenge.Entities
 {
     public class Department : EntityBase, IEntityBase
     {
+        public virtual ICollection<Employee> Employees { get; set; }
+
+        public Department()
+        {
+            Employees = new List<Employee>();
+        }
     }
 }
