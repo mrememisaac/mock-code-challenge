fdf139918b1a61bcd5386f0678c7add7e4a2dca1
Emem Isaac <me@ememisaac.com>
2021-03-22 07:24:38 +0100

    Create an EntityBase class
    This class contains properties common to the Employee and Department Entities

---
+++ VogCodeChallenge.Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace VogCodeChallenge.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
