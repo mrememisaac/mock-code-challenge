7ecc332a7466ec1a4a5bea8005a774be529eaa8f
Emem Isaac <me@ememisaac.com>
2021-03-22 17:05:45 +0100

    Iterate over the names list without for/foreach loops

---
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void Iterate()
+        {
+            var enumerator = NamesList.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
