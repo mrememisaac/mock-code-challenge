129f3243c0bc9eb4a53be62e4a1b7a62b12a31ed
Emem Isaac <me@ememisaac.com>
2021-03-22 08:04:20 +0100

    Implement the IEquatable interface
    This will ensure that we can compare two department entities without casting

--- VogCodeChallenge.Entities/Department.cs
+++ VogCodeChallenge.Entities/Department.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace VogCodeChallenge.Entities
 {
-    public class Department : EntityBase, IEntityBase
+    public class Department : EntityBase, IEntityBase, IEquatable<Department>
     {
         public virtual ICollection<Employee> Employees { get; set; }
 
         public Department()
         {
             Employees = new List<Employee>();
+        }
+
+        public bool Equals([AllowNull] Department other)
+        {
+            return other != null && Id == other.Id;
         }
     }
 }
